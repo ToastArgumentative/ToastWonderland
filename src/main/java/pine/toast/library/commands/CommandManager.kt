17c43fd5230d18fe4b2c8630e0c5c0e356084bc1
ToastARG <toastarg@icloud.com>
2024-04-12 10:52:07 -0400

    Refactor CommandManager for code simplification
    Removed superfluous command creation methods from CommandManager.kt and simplified the way commands are registered and executed. These changes help to streamline the code and improve its readability. In particular, the changes ensure a much cleaner implementation of permission checks, command execution, and exception handling.

--- src/main/java/pine/toast/library/commands/CommandManager.kt
+++ src/main/java/pine/toast/library/commands/CommandManager.kt
@@ -1,8 +1,6 @@
 package pine.toast.library.commands
 
-import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
-import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
@@ -18,55 +16,35 @@ class CommandManager {
 
     private val commands: MutableMap<String, Pair<Method, Any>> = mutableMapOf()
 
-    private fun createCommandAndAddToMap() {
-        val commandMap = Bukkit.getCommandMap()
-        for ((commandName, commandData) in commands) {
-
-            val officialCommand: Command = object : Command(commandName) {
-                override fun execute(sender: CommandSender, commandLabel: String, args: Array<String>): Boolean {
-                    return executeCommand(sender, commandLabel, args)
-                }
-            }
-            // Register your command
-            commandMap.register(Wonderland.getPlugin().name, officialCommand)
-        }
-    }
-
-
     fun registerCommands(vararg instances: Any) {
         // Clear previous commands
         commands.clear()
 
-        // Iterate over each provided instance to register commands
+        // Register commands from each instance
         instances.forEach { instance ->
             registerCommandsFromInstance(instance)
         }
         registerCommandsFromInstance(Wonderland.getPlugin())
 
         Wonderland.getPlugin().logger.log(Level.INFO, "Registered ${commands.size} commands.")
-        createCommandAndAddToMap()
     }
-
 
     private fun registerCommandsFromInstance(instance: Any) {
         val methods = instance.javaClass.declaredMethods
         for (method in methods) {
-            if (!method.isAnnotationPresent(CommandConsole::class.java) &&
-                !method.isAnnotationPresent(CommandPlayer::class.java) &&
-                !method.isAnnotationPresent(CommandAll::class.java)) continue
+            if (method.isAnnotationPresent(CommandConsole::class.java) ||
+                method.isAnnotationPresent(CommandPlayer::class.java) ||
+                method.isAnnotationPresent(CommandAll::class.java)) {
 
-            val commandName = method.name.lowercase(Locale.getDefault())
-            if (commands.containsKey(commandName)) {
-                Wonderland.getPlugin().logger.log(Level.SEVERE, "Warning: Duplicate command '$commandName'. Skipping...")
-                continue
+                val commandName = method.name.lowercase(Locale.getDefault())
+                if (commands.containsKey(commandName)) {
+                    Wonderland.getPlugin().logger.log(Level.SEVERE, "Warning: Duplicate command '$commandName'. Skipping...")
+                    continue
+                }
+                commands[commandName] = Pair(method, instance)
             }
-
-
-            commands[commandName] = Pair(method, instance)
-
         }
     }
-
 
     fun executeCommand(sender: CommandSender, label: String, args: Array<String>): Boolean {
         val (method, instance) = commands[label.lowercase(Locale.getDefault())] ?: return false
@@ -75,89 +53,43 @@ class CommandManager {
         val isPlayerCommand = method.isAnnotationPresent(CommandPlayer::class.java)
         val isAllCommand = method.isAnnotationPresent(CommandAll::class.java)
 
-        // If the command is marked as console-only and the sender is not the console, return true to hide command usage print
         if (isConsoleCommand && sender !is ConsoleCommandSender) {
             sender.sendMessage(WonderlandColors.RED + "This command is only available in the console.")
-            Wonderland.getPlugin().logger.log(
-                Level.WARNING,
-                "${sender.name} has tried to execute $label but does not have permission to do so."
-            )
             return true
         }
 
-        // If the command is marked as player-only and the sender is not a player, return false
         if (isPlayerCommand && sender !is Player) {
-            Wonderland.getPlugin().logger.log(Level.WARNING, "This command is only available for players.")
+            sender.sendMessage(WonderlandColors.RED + "This command is only available for players.")
             return false
         }
 
-        // If the command is marked as CommandAll check whether the sender is a player or the console
-        if (isAllCommand && (sender is Player || sender is ConsoleCommandSender)) {
-            val annotation = method.getAnnotation(CommandAll::class.java) ?: return false
-            val permission = annotation.permission
-            val cooldownSeconds = annotation.cooldown
-
-            if (permission.isNotEmpty() && !sender.hasPermission(permission)) {
-                Wonderland.getPlugin().logger.log(
-                    Level.SEVERE,
-                    "${sender.name} has tried to execute $label but does not have permission to do so."
-                )
+        if (isAllCommand) {
+            val annotation = method.getAnnotation(CommandAll::class.java)
+            if (!sender.hasPermission(annotation.permission)) {
+                sender.sendMessage(WonderlandColors.RED + "You do not have permission to perform this command.")
                 return true
             }
 
-            if (cooldownSeconds > 0 && sender is Player) {
+            if (annotation.cooldown > 0 && sender is Player) {
                 val cooldownKey = NamespacedKey(Wonderland.getPlugin(), label)
                 if (CooldownManager.isPlayerOnCooldown(sender, cooldownKey)) {
                     val remainingCooldown = CooldownManager.getPlayerRemainingCooldown(sender, cooldownKey)
                     sender.sendMessage(WonderlandColors.RED + "You are still on cooldown for this command. Time remaining: ${remainingCooldown / 1000} seconds")
                     return true
                 } else {
-                    CooldownManager.applyPlayerCooldown(sender, cooldownKey, cooldownSeconds)
+                    CooldownManager.applyPlayerCooldown(sender, cooldownKey, annotation.cooldown)
                 }
             }
         }
 
-        if (isPlayerCommand && sender is Player) {
-            val annotation = method.getAnnotation(CommandPlayer::class.java)
-            val permission = annotation.permission
-            val cooldownSeconds = annotation.cooldown
-
-            if (permission.isNotEmpty() && !sender.hasPermission(permission)) {
-                Wonderland.getPlugin().logger.log(
-                    Level.SEVERE,
-                    "${sender.name} has tried to execute $label but does not have permission to do so."
-                )
-                return true
-            }
-
-            if (cooldownSeconds > 0) {
-                val cooldownKey = NamespacedKey(Wonderland.getPlugin(), label)
-                if (CooldownManager.isPlayerOnCooldown(sender, cooldownKey)) {
-                    val remainingCooldown = CooldownManager.getPlayerRemainingCooldown(sender, cooldownKey)
-                    sender.sendMessage(WonderlandColors.RED + "You are still on cooldown for this command. Time remaining: ${remainingCooldown / 1000} seconds")
-                    return true
-                } else {
-                    CooldownManager.applyPlayerCooldown(sender, cooldownKey, cooldownSeconds)
-                }
-            }
-        }
-
-        // Execute the command
-        try {
-            method.invoke(instance, sender, args)
+        return try {
+            method.invoke(instance, sender, args) as Boolean
+        } catch (e: IllegalAccessException) {
+            Wonderland.getPlugin().logger.log(Level.SEVERE, "Illegal access while executing command '$label'", e)
+            false
         } catch (e: InvocationTargetException) {
-            // If the method throws an exception, it's wrapped in InvocationTargetException
-            if (e.targetException != null) {
-                // Log the actual cause of the method exception
-                Wonderland.getPlugin().logger.log(Level.SEVERE, "Error executing command '$label': ${e.targetException.message}")
-            } else {
-                // This might be unnecessary if you're sure the command executed successfully
-                Wonderland.getPlugin().logger.log(Level.SEVERE, "Error executing command '$label': null")
-            }
+            Wonderland.getPlugin().logger.log(Level.SEVERE, "Error executing command '$label': ${e.targetException?.message}", e)
+            false
         }
-
-        return true
     }
-
-
 }
